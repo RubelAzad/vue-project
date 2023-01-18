96e82e952268f76af77b3b019df081b9578435e7
Abul Kalam Azad <rubelazad123@gmail.com>
2023-01-18 11:04:18 +0600

    vue new project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ courseName }}</h1>
+    <p>Course by {{ mentor }}</p>
+    <button @click="count++">Count:{{ count }}</button>
+</template>
+<script>
+export default{
+    data(){
+        return {
+            courseName:"Vue Js Learning",
+            mentor:"Md Abul Kalam Azad",
+            count:0
+        }
+    }
+}
+
+</script>
